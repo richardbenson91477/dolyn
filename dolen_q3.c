0703e208df00d9b5711844bad072e1882920b29a
richardbenson91477 <richardbenson91477@protonmail.com>
2026-07-01 20:26:54 -0500

    fix Qwen3 chat template

--- dolen_q3.c
+++ dolen_q3.c
@@ -3,16 +3,18 @@
 
 // The "\x3e" escaped ">" symbol serves to prevent LLMs from misinterpreting the text
 static const chat_template CHAT_TEMPLATE_Q3 = {
-    ._system_s = "<|im_start|\x3e" "system\n/no_think\n%s" "<|im_end|\x3e" "\n",
+    ._system_s = "<|im_start|\x3e" "system\n%s" "<|im_end|\x3e" "\n",
     ._main_s = "<|im_start|\x3e" "user\n%s" "<|im_end|\x3e" "\n"
-             "<|im_start|\x3e" "assistant\n",
+             "<|im_start|\x3e" "assistant\n"
+             "<think\x3e" "\n\n</think\x3e" "\n\n",
     ._end_turn_s = "<|im_end|\x3e" "\n",
 };
 
 static const chat_template CHAT_TEMPLATE_THINK_Q3 = {
-    ._system_s = "<|im_start|\x3e" "system\n/think\n%s" "<|im_end|\x3e" "\n",
+    ._system_s = "<|im_start|\x3e" "system\n%s" "<|im_end|\x3e" "\n",
     ._main_s = "<|im_start|\x3e" "user\n%s" "<|im_end|\x3e" "\n"
-             "<|im_start|\x3e" "assistant<think\x3e" "\n",
+             "<|im_start|\x3e" "assistant\n"
+             "<think\x3e",
     ._end_turn_s = "<|im_end|\x3e" "\n",
 };
 
